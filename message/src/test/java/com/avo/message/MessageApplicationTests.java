914462993fa607d36dc0500a2e09c1ae208de7a2
jayadurga-t <jayadurga.t332@gmail.com>
2026-04-14 16:02:16 +0530

    chore(message): initialize Message project

---
+++ message/src/test/java/com/avo/message/MessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avo.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
